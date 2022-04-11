6ebe431ffcf69ff27cbd621d0190d719c1f8975f
Ivan <m.i.r.2009@mail.ru>
2022-04-11 18:55:21 +0300

    Show table added

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Добро пожаловать в игру!")
 username = input("Пожалуйста, представьтесь: ")
+print("Вам надо отгадать слова")
+
+answer = "ТОКИО"
+
+word = []
+
+for i in range(len(answer)):
+    word.append('*')
+
+print(*word)
